725b6bdd95348f312f28bcd8cf97e635845c1850
limarman <limarman@googlemail.com>
2023-10-27 12:57:37 +0200

    Updated files.

---
+++ create_table.js
@@ -0,0 +1,17 @@
+// URL to the JSON file
+const jsonUrl = 'eval_latest.json';
+
+// Get the div element
+const jsonDataDiv = document.getElementById('json-data');
+
+// Fetch JSON data from the file
+fetch(jsonUrl)
+    .then(response => response.json())
+    .then(data => {
+        // Update the content of the div with JSON data
+        jsonDataDiv.textContent = `Group: 2, Score: ${data.groups.2.mean}`;
+    })
+    .catch(error => {
+        // Handle errors, e.g., if the JSON file doesn't exist
+        console.error('Error loading JSON data:', error);
+    });
