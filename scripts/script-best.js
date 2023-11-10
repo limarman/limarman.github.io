5652eb9c8ad5688ae8fd0c35238b33d32c6ebfe1
limarman <limarman@googlemail.com>
2023-11-10 16:08:12 +0100

    Updated files.

--- scripts/script-best.js
+++ scripts/script-best.js
@@ -1,8 +1,74 @@
 // URL to the JSON file
 const jsonUrlBest = 'eval_best.json';
 
+const json_urls = ['eval_best.json', 'group_names.json']
+
+const promises = json_urls.map(url =>
+    fetch(url)
+      .then(response => response.json())
+      .catch(error => console.error('Error loading JSON:', error))
+  );
+
+// Get the div element for the table
 const tableDiv = document.getElementById('data-table');
 
+document.addEventListener('DOMContentLoaded', function() {
+    Promise.all(promises)
+        .then(dataArray => {
+            const data = dataArray[0]
+            const group_names = dataArray[1]
+
+            // Access the "groups" object within the JSON data
+            const groups = data.groups;
+
+            // Convert the groups object into an array of objects
+            const groupArray = Object.entries(groups).map(([groupNumber, groupData]) => {
+                return { groupNumber, mean: groupData.mean, error: groupData.error};
+            });
+
+            groupArray.sort((a, b) => {
+                if (a.error && !b.error) {
+                    return 1; // Place groups with errors below groups without errors
+                } else if (!a.error && b.error) {
+                    return -1; // Place groups without errors above groups with errors
+                }
+                else{
+                    return b.mean - a.mean;
+                }
+            });
+
+            // Select the HTML table element by its ID
+            const table = document.getElementById('data-table');
+
+            // Iterate over the groups and create table rows
+            groupArray.forEach((group, index) => {
+                const row = table.insertRow(); // Insert a new row into the table
+                const cell1 = row.insertCell(); // Insert cells for group number and mean
+                const cell2 = row.insertCell();
+                const cell3 = row.insertCell();
+                const cell4 = row.insertCell();
+
+                // Assign data to the cells
+                //cell1.textContent = groupNumber;
+                cell1.textContent = (index + 1).toString()
+                cell2.textContent = group.groupNumber
+                if(group.error)
+                {
+                    cell3.textContent = 'ERROR';
+                    cell3.classList.add('error-eval')
+                }
+                else{
+                    cell3.textContent = group.mean.toFixed(3);
+                }
+
+            });
+        })
+        .catch(error => console.error('Error loading JSON:', error));
+})
+
+
+
+/*
 document.addEventListener('DOMContentLoaded', function() {
     // Fetch JSON data from the file
     fetch(jsonUrlBest)
@@ -56,3 +122,4 @@ document.addEventListener('DOMContentLoaded', function() {
             console.error('Error loading JSON data:', error);
         });
 });
+*/
--- scripts/script-latest.js
+++ scripts/script-latest.js
@@ -2,9 +2,87 @@
 const jsonUrl = 'eval_latest.json';
 const jsonUrlBest = 'eval_best.json';
 
+const json_urls = ['eval_latest.json', 'eval_best.json', 'group_names.json']
+
+const promises = json_urls.map(url =>
+    fetch(url)
+      .then(response => response.json())
+      .catch(error => console.error('Error loading JSON:', error))
+  );
+
 // Get the div element where you want to display the timestamp
 const timestampDiv = document.getElementById('latest-timestamp');
 
+// Get the div element for the table
+const tableDiv = document.getElementById('data-table');
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    Promise.all(promises)
+        .then(dataArray => {
+
+            const data = dataArray[0]
+            const best_data = dataArray[1]
+            const group_names = dataArray[2]
+
+            // setting the correct timestamp
+            const timestamp = data.timestamp;
+            timestampDiv.textContent = `Timestamp: ${timestamp}`;
+            
+            //filling the table
+            // Access the "groups" object within the JSON data
+            const groups = data.groups;
+
+            // Convert the groups object into an array of objects
+            const groupArray = Object.entries(groups).map(([groupNumber, groupData]) => {
+                return { groupNumber, mean: groupData.mean, error: groupData.error};
+            });
+
+            groupArray.sort((a, b) => {
+                if (a.error && !b.error) {
+                    return 1; // Place groups with errors below groups without errors
+                } else if (!a.error && b.error) {
+                    return -1; // Place groups without errors above groups with errors
+                }
+                else{
+                    return b.mean - a.mean;
+                }
+            });
+            // Iterate over the groups and create table rows
+            //Object.keys(groups).forEach(groupNumber => {
+            groupArray.forEach(group => {
+                //const groupData = groups[groupNumber];
+                const row = tableDiv.insertRow(); // Insert a new row into the table
+                const cell1 = row.insertCell(); // Insert cells for group number and mean
+                const cell2 = row.insertCell();
+                const cell3 = row.insertCell();
+
+                // Assign data to the cells
+                //cell1.textContent = groupNumber;
+                cell1.textContent = group.groupNumber;
+                cell2.textContent = group_names[group.groupNumber].name;
+                //cell2.textContent = groupData.mean.toFixed(3);
+                if(group.error)
+                {
+                    cell3.textContent = 'ERROR';
+                    cell3.classList.add('error-eval')
+                }
+                else{
+                    cell3.textContent = group.mean.toFixed(3);
+                    console.log(best_data.groups[group.groupNumber].error)
+                    if(best_data.groups[group.groupNumber].error ||
+                        best_data.groups[group.groupNumber].mean >= group.mean)
+                    {
+                        cell3.classList.add('best-eval')
+                    }
+                }
+            });
+
+        })
+        .catch(error => console.error('Error loading JSON:', error));
+})
+
+/*
 // Fetch JSON data from the file
 fetch(jsonUrl)
     .then(response => response.json())
@@ -20,8 +98,6 @@ fetch(jsonUrl)
         console.error('Error loading JSON data:', error);
     });
 
-
-const tableDiv = document.getElementById('data-table');
 
 document.addEventListener('DOMContentLoaded', function() {
     // Fetch JSON data from the file
@@ -59,23 +135,25 @@ document.addEventListener('DOMContentLoaded', function() {
                         const row = table.insertRow(); // Insert a new row into the table
                         const cell1 = row.insertCell(); // Insert cells for group number and mean
                         const cell2 = row.insertCell();
+                        const cell3 = row.insertCell();
 
                         // Assign data to the cells
                         //cell1.textContent = groupNumber;
                         cell1.textContent = group.groupNumber
+                        cell2.textContent = 
                         //cell2.textContent = groupData.mean.toFixed(3);
                         if(group.error)
                         {
-                            cell2.textContent = 'ERROR';
-                            cell2.classList.add('error-eval')
+                            cell3.textContent = 'ERROR';
+                            cell3.classList.add('error-eval')
                         }
                         else{
-                            cell2.textContent = group.mean.toFixed(3);
+                            cell3.textContent = group.mean.toFixed(3);
                             console.log(best_data.groups[group.groupNumber].error)
                             if(best_data.groups[group.groupNumber].error ||
                                 best_data.groups[group.groupNumber].mean >= group.mean)
                             {
-                                cell2.classList.add('best-eval')
+                                cell3.classList.add('best-eval')
                             }
                         }
                     });
@@ -88,3 +166,4 @@ document.addEventListener('DOMContentLoaded', function() {
             console.error('Error loading JSON data:', error);
         });
 });
+*/
